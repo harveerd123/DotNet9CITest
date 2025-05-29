3ed97d0d7b92ddf0e352ed478abf1828ea837c47
harveerd123 <harveerdole@gmail.com>
2025-05-29 16:39:11 +0100

    Add project files.

---
+++ App.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Tests;
+using Xunit;
+public class UnitTest1
+{
+    public class Tests()
+    {
+        [Fact]
+        public void SayHello_ReturnsExpectedString()
+        {
+            var result = Greeter.SayHello("Test");
+            Assert.Equal("Hello, Test!", result);
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace App;
+public class Greeter
+{
+    public static string SayHello(string name) => $"Hello, {name}!";
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Greeter.SayHello("World"));
+    }
+}
